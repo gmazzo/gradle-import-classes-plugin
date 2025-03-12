79005372f85936b0c76c181c93e145d137febbf7
Guillermo Mazzola <gmazzo65@gmail.com>
2025-03-12 18:12:31 +0100

    Updated CI/CD process to publish to Maven Central

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,33 +2,68 @@ plugins {
     alias(libs.plugins.buildConfig)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.samReceiver)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.axion.release)
+    alias(libs.plugins.mavenPublish)
     alias(libs.plugins.gradle.pluginPublish)
     alias(libs.plugins.publicationsReport)
-    signing
     jacoco
 }
 
 group = "io.github.gmazzo.importclasses"
-description = "Gradle Import Classes Plugin"
-version = providers
-    .exec { commandLine("git", "describe", "--tags", "--always") }
-    .standardOutput.asText.get().trim().removePrefix("v")
+description = "Imports and repackages dependencies using `Proguard` tool"
+version = scmVersion.version
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get()))
 kotlin.compilerOptions.freeCompilerArgs.add("-Xjvm-default=all")
 samWithReceiver.annotation(HasImplicitReceiver::class.qualifiedName!!)
 
+val originUrl = providers
+    .exec { commandLine("git", "remote", "get-url", "origin") }
+    .standardOutput.asText.map { it.trim() }
+
 gradlePlugin {
-    website.set("https://github.com/gmazzo/gradle-import-classes-plugin")
-    vcsUrl.set("https://github.com/gmazzo/gradle-import-classes-plugin")
+    website = originUrl
+    vcsUrl = originUrl
 
     plugins {
         create("import-classes") {
             id = "io.github.gmazzo.importclasses"
             displayName = name
             implementationClass = "io.github.gmazzo.importclasses.ImportClassesPlugin"
-            description = "Imports and repackages dependencies using `Proguard` tool"
+            description = project.description
             tags.addAll("jarjar", "proguard", "dependencies", "repackage", "import", "classes")
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral("CENTRAL_PORTAL", automaticRelease = true)
+
+    pom {
+        name = "${rootProject.name}-${project.name}"
+        description = provider { project.description }
+        url = originUrl
+
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/license/mit/"
+            }
+        }
+
+        developers {
+            developer {
+                id = "gmazzo"
+                name = id
+                email = "gmazzo65@gmail.com"
+            }
+        }
+
+        scm {
+            connection = originUrl
+            developerConnection = originUrl
+            url = originUrl
         }
     }
 }
@@ -51,15 +86,6 @@ dependencies {
     testImplementation(plugin(libs.plugins.kotlin.jvm))
 }
 
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
-    isRequired = signingKey != null || providers.environmentVariable("GRADLE_PUBLISH_KEY").isPresent
-}
-
 testing.suites.withType<JvmTestSuite> {
     useJUnitJupiter()
 }
@@ -70,6 +96,20 @@ tasks.test {
 
 tasks.jacocoTestReport {
     reports.xml.required = true
+}
+
+afterEvaluate {
+    tasks.named<Jar>("javadocJar") {
+        from(tasks.dokkaGeneratePublicationJavadoc)
+    }
+}
+
+tasks.withType<PublishToMavenRepository>().configureEach {
+    mustRunAfter(tasks.publishPlugins)
+}
+
+tasks.publishPlugins {
+    enabled = "$version".matches("\\d+(\\.\\d+)+".toRegex())
 }
 
 tasks.publish {
