c34a5bda0f8bd5305d87e3fc24ad0f216d64f96c
Guillermo Mazzola <gmazzo65@gmail.com>
2025-11-07 16:20:43 +0100

    Adding public API check

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,3 +1,7 @@
+@file:OptIn(ExperimentalAbiValidation::class)
+
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
+
 plugins {
     alias(libs.plugins.buildConfig)
     alias(libs.plugins.kotlin.jvm)
@@ -15,6 +19,7 @@ description = "Imports and repackages dependencies using `Proguard` tool"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get()))
 samWithReceiver.annotation(HasImplicitReceiver::class.qualifiedName!!)
+kotlin.abiValidation.enabled = true
 
 val originUrl = providers
     .exec { commandLine("git", "remote", "get-url", "origin") }
@@ -105,6 +110,14 @@ afterEvaluate {
 
 tasks.withType<PublishToMavenRepository>().configureEach {
     mustRunAfter(tasks.publishPlugins)
+}
+
+tasks.validatePlugins {
+    enableStricterValidation = true
+}
+
+tasks.check {
+    dependsOn(tasks.checkLegacyAbi)
 }
 
 tasks.publishPlugins {
